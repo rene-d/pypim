3913acecff9c9bd99226bf22fb949fbaf768b8b5
René Devichi <rene.devichi@gmail.com>
2019-07-12 05:26:24 +0200

    json api

--- json-package.sh
+++ json-package.sh
@@ -18,7 +18,7 @@ while getopts ":hvf:r" option; do
             ;;
         *)
             echo "Returns metadata (info) about an individual project at the latest version."
-            echo "Usage: $0 [options] <project_name>"
+            echo "Usage: $0 [options] <project_name> [<version>]"
             echo "Options"
             echo "  -f EXPR : jq filter"
             echo "  -r      : don't pipe JSON through jq filter"
@@ -30,8 +30,14 @@ done
 shift $((OPTIND-1))
 
 # jq is required to filter
-[[ $filter ]] && /usr/bin/which jq >/dev/null || unset filter
+[[ ${filter} ]] && /usr/bin/which jq >/dev/null || unset filter
 
-curl -L ${verbose} -s -H 'Content-Type: application/json' https://pypi.org/pypi/$1/json | (
+if [[ $2 ]]; then
+    url=https://pypi.org/pypi/$1/$2/json
+else
+    url=https://pypi.org/pypi/$1/json
+fi
+
+curl -L ${verbose} -s -H 'Content-Type: application/json' ${url} | (
     [[ ${filter} ]] && jq ${filter} || (cat; echo)
 )
